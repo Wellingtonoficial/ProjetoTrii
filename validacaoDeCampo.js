abf95fdac3cc80a9387fdca823ac6810fa243940
Wellington <wellingtonoficial00@gmail.com>
2022-01-29 15:28:26 -0300

    mm

---
+++ validacaoDeCampo.js
@@ -0,0 +1,40 @@
+
+document.getElementById("btn").addEventListener('click', (e, succ)=> {
+    e.preventDefault()
+    const nome = document.getElementById("nome")
+    const email = document.getElementById("email")
+    const tel = document.getElementById("tel")
+    const servicos = document.getElementById("servicos")
+    const mensagem = document.getElementById("mensagem")
+
+    if(nome.value == ""){
+        nome.classList.toggle("focusErro")
+        nome.focus()
+    }
+    else if(email.value == ""){
+        email.classList.toggle("focusErro")
+        email.focus()
+    }
+    else if (email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1 || (email.value.indexOf(".") - email.value.indexOf("@") == 1)){
+        email.classList.toggle("focusErro")
+        email.focus()
+    }
+    else if(tel.value == ""){
+        tel.classList.toggle("focusErro")
+        tel.focus()
+    }
+    else if(servicos.value == ""){
+        servicos.classList.toggle("focusErro")
+        servicos.focus()
+    }
+    else if(mensagem.value == ""){
+        mensagem.classList.toggle("focusErro")
+        mensagem.focus()
+    }
+    else{        
+        let msgSuces  = document.getElementById("msgSuces").innerHTML = "Mensagem enviada com sucesso!"
+            
+    }
+
+})
+
